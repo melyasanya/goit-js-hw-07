17597bed4786bab650ad154b14ff1f0061c12fed
melyasanya <werbolom@gmail.com>
2023-04-27 16:52:28 +0300

    without esc

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+const makeGallery = (item) => {
+  const listItem = document.createElement("li");
+  list.classList.add("gallery__item");
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.setAttribute("href", `${item.original}`);
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.setAttribute("src", `${item.preview}`);
+  image.setAttribute("data-source", `${item.original}`);
+  image.setAttribute("alt", `${item.description}`);
+  link.append(image);
+  listItem.append(link);
+  link.addEventListener("click", () => {
+    link.removeAttribute("href");
+  });
+
+  listItem.addEventListener("click", () => {
+    const instance = basicLightbox.create(`
+<img src="${image.dataset.source}">
+`);
+    instance.show();
+  });
+  return listItem;
+};
+
+const gallery = galleryItems.map(makeGallery);
+
+list.append(...gallery);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+const makeGallery = (item) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery__item");
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.setAttribute("href", `${item.original}`);
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.setAttribute("src", `${item.preview}`);
+  image.setAttribute("alt", `${item.description}`);
+  image.setAttribute("title", `${item.description}`);
+  link.append(image);
+  listItem.append(link);
+
+  return listItem;
+};
+
+const gallery = galleryItems.map(makeGallery);
+
+list.append(...gallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
